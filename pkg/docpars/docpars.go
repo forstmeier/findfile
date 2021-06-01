fdc92a805fc0e71f9d821c22a8674a58aeca13b7
forstmeier <john.forstmeier@gmail.com>
2021-05-31 20:56:12 -0400

    Add JSON tags to docpars structs

--- pkg/docpars/docpars.go
+++ pkg/docpars/docpars.go
@@ -4,29 +4,29 @@ import "context"
 
 // Content holds the output of parsing the provided image file.
 type Content struct {
-	ID    string
-	Lines []Data
-	Words []Data
+	ID    string `json:"id"`
+	Lines []Data `json:"lines"`
+	Words []Data `json:"words"`
 }
 
 // Data holds text and location coordinates retrieved from the image file.
 type Data struct {
-	Text        string
-	Coordinates Coordinates
+	Text        string      `json:"text"`
+	Coordinates Coordinates `json:"coordinates"`
 }
 
 // Coordinates holds the four coordinate points for a piece of text.
 type Coordinates struct {
-	TopLeft     Coordinate
-	TopRight    Coordinate
-	BottomLeft  Coordinate
-	BottomRight Coordinate
+	TopLeft     Coordinate `json:"top_left"`
+	TopRight    Coordinate `json:"top_right"`
+	BottomLeft  Coordinate `json:"bottom_left"`
+	BottomRight Coordinate `json:"bottom_right"`
 }
 
 // Coordinate holds the X and Y values for a point in text coordinates.
 type Coordinate struct {
-	X float64
-	Y float64
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
 }
 
 // Parser defines the method needed for converting the provided
