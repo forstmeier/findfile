b2c9f6a026e092839c4cb6b5293df1093afcab65
forstmeier <john.forstmeier@gmail.com>
2021-06-20 18:00:18 -0400

    Build initial fileslambda package

---
+++ cmd/fileslambda/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/cheesesteakio/api/pkg/acct"
+	"github.com/cheesesteakio/api/pkg/fs"
+)
+
+const accountIDHeader = "x-cheesesteakstorage-account-id"
+
+func handler(fsClient fs.Filesystemer, acctClient acct.Accounter) func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		accountID, ok := request.Headers[accountIDHeader]
+		if !ok {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       `{"error": "account id not provided"}`,
+			}, nil
+		}
+
+		account, err := acctClient.ReadAccount(ctx, accountID)
+
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Body:       `{"error": "error getting account values"}`,
+			}, nil
+		}
+
+		if account == nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Body:       fmt.Sprintf(`{"error": "account [%s] not found}`, accountID),
+			}, nil
+		}
+
+		filenames := []string{}
+		if err := json.Unmarshal([]byte(request.Body), &filenames); err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Body:       `{"error": "error unmarshalling filenames array"}`,
+			}, nil
+		}
+
+		body := ""
+
+		switch request.HTTPMethod {
+		case http.MethodPost:
+			presignedURLs := make([]string, len(filenames))
+			for i, fileName := range filenames {
+				presignedURL, err := fsClient.GenerateUploadURL(ctx, accountID, fileName)
+				if err != nil {
+					return events.APIGatewayProxyResponse{
+						StatusCode: http.StatusInternalServerError,
+						Body:       fmt.Sprintf(`{"error": "error generating [%s] presigned url"}`, fileName),
+					}, nil
+				}
+
+				presignedURLs[i] = presignedURL
+			}
+
+			presignedURLsBytes, err := json.Marshal(presignedURLs)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error marshalling presigned urls"}`,
+				}, nil
+			}
+
+			body = fmt.Sprintf(`{"message": "success", "urls": %s}`, presignedURLsBytes)
+		case http.MethodDelete:
+			if err := fsClient.DeleteFiles(ctx, accountID, filenames); err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error deleting files"}`,
+				}, nil
+			}
+
+			body = `{"message": "success"}`
+		default:
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       fmt.Sprintf(`{"error": "http method [%s] not supported"}`, request.HTTPMethod),
+			}, nil
+		}
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       body,
+		}, nil
+	}
+}
+
+func main() {
+	fsClient, err := fs.New()
+	if err != nil {
+		log.Fatalf("error creating filesystem client: %s", err.Error())
+	}
+
+	acctClient := acct.New()
+
+	lambda.Start(handler(fsClient, acctClient))
+}
---
+++ cmd/fileslambda/main_test.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/cheesesteakio/api/pkg/acct"
+)
+
+type mockFSClient struct {
+	mockGeneratePresignedURLOutput string
+	mockGeneratePresignedURLError  error
+	mockDeleteFilesError           error
+}
+
+func (m *mockFSClient) GenerateUploadURL(ctx context.Context, accountID string, filename string) (string, error) {
+	return m.mockGeneratePresignedURLOutput, m.mockGeneratePresignedURLError
+}
+
+func (m *mockFSClient) DeleteFiles(ctx context.Context, accountID string, filenames []string) error {
+	return m.mockDeleteFilesError
+}
+
+type mockAcctClient struct {
+	mockReadAccountOutput *acct.Account
+	mockReadAccountError  error
+}
+
+func (m *mockAcctClient) CreateAccount(ctx context.Context, accountID string) error {
+	return nil
+}
+
+func (m *mockAcctClient) ReadAccount(ctx context.Context, accountID string) (*acct.Account, error) {
+	return m.mockReadAccountOutput, m.mockReadAccountError
+}
+
+func (m *mockAcctClient) UpdateAccount(ctx context.Context, accountID string, values map[string]string) error {
+	return nil
+}
+
+func (m *mockAcctClient) DeleteAccount(ctx context.Context, accountID string) error {
+	return nil
+}
+
+func Test_handler(t *testing.T) {
+	tests := []struct {
+		description                    string
+		request                        events.APIGatewayProxyRequest
+		mockGeneratePresignedURLOutput string
+		mockGeneratePresignedURLError  error
+		mockDeleteFilesError           error
+		mockReadAccountOutput          *acct.Account
+		mockReadAccountError           error
+		statusCode                     int
+		body                           string
+	}{
+		{
+			description:                    "no account id in request",
+			request:                        events.APIGatewayProxyRequest{},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          nil,
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusBadRequest,
+			body:                           `{"error": "account id not provided"}`,
+		},
+		{
+			description: "dynamodb client error reading account from database",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPost,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          nil,
+			mockReadAccountError:           errors.New("mock read account error"),
+			statusCode:                     http.StatusInternalServerError,
+			body:                           `{"error": "error getting account values"}`,
+		},
+		{
+			description: "dynamodb client account not found in database",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPost,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          nil,
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusInternalServerError,
+			body:                           `{"error": "account [account_id] not found}`,
+		},
+		{
+			description: "unmarshal received file names",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPost,
+				Body:       "---------",
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusInternalServerError,
+			body:                           `{"error": "error unmarshalling filenames array"}`,
+		},
+		{
+			description: "unsupported http method",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPatch,
+				Body:       `["filename.jpg"]`,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusBadRequest,
+			body:                           `{"error": "http method [PATCH] not supported"}`,
+		},
+		{
+			description: "s3 client error presigning upload urls",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPost,
+				Body:       `["filename.jpg"]`,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  errors.New("mock generate presigned url error"),
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusInternalServerError,
+			body:                           `{"error": "error generating [filename.jpg] presigned url"}`,
+		},
+		{
+			description: "successful post handler invocation",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodPost,
+				Body:       `["filename.jpg"]`,
+			},
+			mockGeneratePresignedURLOutput: "https://s3.amazonaws.com/bucket/account_id/filename.jpg",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusOK,
+			body:                           `{"message": "success", "urls": ["https://s3.amazonaws.com/bucket/account_id/filename.jpg"]}`,
+		},
+		{
+			description: "s3 client delete objects error",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodDelete,
+				Body:       `["filename.jpg"]`,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           errors.New("mock delete object error"),
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusInternalServerError,
+			body:                           `{"error": "error deleting files"}`,
+		},
+		{
+			description: "successful delete handler invocation",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+				HTTPMethod: http.MethodDelete,
+				Body:       `["filename.jpg"]`,
+			},
+			mockGeneratePresignedURLOutput: "",
+			mockGeneratePresignedURLError:  nil,
+			mockDeleteFilesError:           nil,
+			mockReadAccountOutput:          &acct.Account{},
+			mockReadAccountError:           nil,
+			statusCode:                     http.StatusOK,
+			body:                           `{"message": "success"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			fsClient := &mockFSClient{
+				mockGeneratePresignedURLOutput: test.mockGeneratePresignedURLOutput,
+				mockGeneratePresignedURLError:  test.mockGeneratePresignedURLError,
+				mockDeleteFilesError:           test.mockDeleteFilesError,
+			}
+
+			acctClient := &mockAcctClient{
+				mockReadAccountOutput: test.mockReadAccountOutput,
+				mockReadAccountError:  test.mockReadAccountError,
+			}
+
+			handlerFunc := handler(fsClient, acctClient)
+
+			response, _ := handlerFunc(context.Background(), test.request)
+
+			if response.StatusCode != test.statusCode {
+				t.Errorf("incorrect status code, received: %d, expected: %d", response.StatusCode, test.statusCode)
+			}
+
+			if response.Body != test.body {
+				t.Errorf("incorrect body, received: %s, expected: %s", response.Body, test.body)
+			}
+		})
+	}
+}
