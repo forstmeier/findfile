2c1314e6186ece4934542fabc0e4c9cf7caf52c8
forstmeier <john.forstmeier@gmail.com>
2021-06-25 09:15:20 -0400

    Build initial acctlambda package

---
+++ cmd/acctlambda/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/uuid"
+
+	"github.com/cheesesteakio/api/pkg/acct"
+	"github.com/cheesesteakio/api/pkg/subscr"
+)
+
+const accountIDHeader = "x-cheesesteakstorage-account-id"
+
+func handler(acctClient acct.Accounter, subscrClient subscr.Subscriber) func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		body := ""
+
+		switch request.HTTPMethod {
+		case http.MethodPost:
+			subscriberInfo := subscr.SubscriberInfo{}
+			if err := json.Unmarshal([]byte(request.Body), &subscriberInfo); err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error unmarshalling subscriber info"}`,
+				}, nil
+			}
+			subscriberInfo.ID = uuid.NewString()
+
+			accountID := uuid.NewString()
+
+			if err := acctClient.CreateAccount(ctx, accountID); err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error creating user account"}`,
+				}, nil
+			}
+
+			_, err := subscrClient.CreateSubscription(ctx, subscriberInfo)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error creating user subscription"}`,
+				}, nil
+			}
+
+			body = fmt.Sprintf(`{"message": "success", "account_id": "%s"}`, accountID)
+
+		case http.MethodDelete:
+			accountID, ok := request.Headers[accountIDHeader]
+			if !ok {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusBadRequest,
+					Body:       `{"error": "account id not provided"}`,
+				}, nil
+			}
+
+			account, err := acctClient.ReadAccount(ctx, accountID)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error getting account values"}`,
+				}, nil
+			}
+
+			if err := subscrClient.RemoveSubscription(ctx, account.StripeCustomerID); err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error removing user subscription"}`,
+				}, nil
+			}
+
+			if err := acctClient.DeleteAccount(ctx, accountID); err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusInternalServerError,
+					Body:       `{"error": "error removing user account"}`,
+				}, nil
+			}
+
+			body = fmt.Sprintf(`{"message": "success", "account_id": "%s"}`, accountID)
+
+		default:
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       fmt.Sprintf(`{"error": "http method [%s] not supported"}`, request.HTTPMethod),
+			}, nil
+		}
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       body,
+		}, nil
+	}
+}
+
+// outline:
+// [ ] "cleanup" function
+// - [ ] calls "delete" methods
+// - - [ ] account
+// - - [ ] subscription
+// - [ ] called on err != nil
+
+func main() {
+	acctClient := acct.New()
+
+	subscrClient := subscr.New()
+
+	lambda.Start(handler(acctClient, subscrClient))
+}
---
+++ cmd/acctlambda/main_test.go
@@ -0,0 +1,277 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/cheesesteakio/api/pkg/acct"
+	"github.com/cheesesteakio/api/pkg/subscr"
+)
+
+type mockAcctClient struct {
+	mockCreateAccountError error
+	mockReadAccountOutput  *acct.Account
+	mockReadAccountError   error
+	mockDeleteAccountError error
+}
+
+func (m *mockAcctClient) CreateAccount(ctx context.Context, accountID string) error {
+	return m.mockCreateAccountError
+}
+
+func (m *mockAcctClient) ReadAccount(ctx context.Context, accountID string) (*acct.Account, error) {
+	return m.mockReadAccountOutput, m.mockReadAccountError
+}
+
+func (m *mockAcctClient) UpdateAccount(ctx context.Context, accountID string, values map[string]string) error {
+	return nil
+}
+
+func (m *mockAcctClient) DeleteAccount(ctx context.Context, accountID string) error {
+	return m.mockDeleteAccountError
+}
+
+type mockSubscrClient struct {
+	mockCreateSubscriptionOutput *subscr.Subscription
+	mockCreateSubscriptionError  error
+	mockRemoveSubscriptionError  error
+}
+
+func (m *mockSubscrClient) CreateSubscription(ctx context.Context, info subscr.SubscriberInfo) (*subscr.Subscription, error) {
+	return m.mockCreateSubscriptionOutput, m.mockCreateSubscriptionError
+}
+
+func (m *mockSubscrClient) RemoveSubscription(ctx context.Context, id string) error {
+	return m.mockRemoveSubscriptionError
+}
+
+func (m *mockSubscrClient) AddUsage(ctx context.Context, id string) error {
+	return nil
+}
+
+func Test_handler(t *testing.T) {
+	tests := []struct {
+		description                  string
+		request                      events.APIGatewayProxyRequest
+		mockCreateAccountError       error
+		mockReadAccountOutput        *acct.Account
+		mockReadAccountError         error
+		mockDeleteAccountError       error
+		mockCreateSubscriptionOutput *subscr.Subscription
+		mockCreateSubscriptionError  error
+		mockRemoveSubscriptionError  error
+		statusCode                   int
+		body                         string
+	}{
+		{
+			description: "unsupported http method recieved",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodGet,
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusBadRequest,
+			body:                         `{"error": "http method \[GET\] not supported"}`,
+		},
+		{
+			description: "error unmarshalling user information",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodPost,
+				Body:       "---------",
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error unmarshalling subscriber info"}`,
+		},
+		{
+			description: "error creating user account",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodPost,
+				Body:       `{"email": "test@email.com"}`,
+			},
+			mockCreateAccountError:       errors.New("mock create account error"),
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error creating user account"}`,
+		},
+		{
+			description: "error creating user subscription",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodPost,
+				Body:       `{"email": "test@email.com"}`,
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  errors.New("mock create subscription error"),
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error creating user subscription"}`,
+		},
+		{
+			description: "successful handler create invocation",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodPost,
+				Body:       `{"email": "test@email.com"}`,
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusOK,
+			body:                         `{"message": "success", "account_id": ".*"}`,
+		},
+		{
+			description: "account id not provided in request header",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodDelete,
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusBadRequest,
+			body:                         `{"error": "account id not provided"}`,
+		},
+		{
+			description: "dynamodb client error reading account from database",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodDelete,
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+			},
+			mockCreateAccountError:       nil,
+			mockReadAccountOutput:        nil,
+			mockReadAccountError:         errors.New("mock read account error"),
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error getting account values"}`,
+		},
+		{
+			description: "stripe client error deleting user subscription",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodDelete,
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+			},
+			mockCreateAccountError: nil,
+			mockReadAccountOutput: &acct.Account{
+				StripeCustomerID: "stripe_customer_id",
+			},
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  errors.New("mock delete subscription error"),
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error removing user subscription"}`,
+		},
+		{
+			description: "dynamodb client error removing account from database",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodDelete,
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+			},
+			mockCreateAccountError: nil,
+			mockReadAccountOutput: &acct.Account{
+				StripeCustomerID: "stripe_customer_id",
+			},
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       errors.New("mock delete account error"),
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusInternalServerError,
+			body:                         `{"error": "error removing user account"}`,
+		},
+		{
+			description: "successful handler delete invocation",
+			request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodDelete,
+				Headers: map[string]string{
+					accountIDHeader: "account_id",
+				},
+			},
+			mockCreateAccountError: nil,
+			mockReadAccountOutput: &acct.Account{
+				StripeCustomerID: "stripe_customer_id",
+			},
+			mockReadAccountError:         nil,
+			mockDeleteAccountError:       nil,
+			mockCreateSubscriptionOutput: nil,
+			mockCreateSubscriptionError:  nil,
+			mockRemoveSubscriptionError:  nil,
+			statusCode:                   http.StatusOK,
+			body:                         `{"message": "success", "account_id": "account_id"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			acctClient := &mockAcctClient{
+				mockCreateAccountError: test.mockCreateAccountError,
+				mockReadAccountOutput:  test.mockReadAccountOutput,
+				mockReadAccountError:   test.mockReadAccountError,
+				mockDeleteAccountError: test.mockDeleteAccountError,
+			}
+
+			subscrClient := &mockSubscrClient{
+				mockCreateSubscriptionOutput: test.mockCreateSubscriptionOutput,
+				mockCreateSubscriptionError:  test.mockCreateSubscriptionError,
+				mockRemoveSubscriptionError:  test.mockRemoveSubscriptionError,
+			}
+
+			handlerFunc := handler(acctClient, subscrClient)
+
+			response, _ := handlerFunc(context.Background(), test.request)
+
+			if response.StatusCode != test.statusCode {
+				t.Errorf("incorrect status code, received: %d, expected: %d", response.StatusCode, test.statusCode)
+			}
+
+			matched, err := regexp.MatchString(test.body, response.Body)
+			if err != nil {
+				t.Fatalf("error matching body regexp: %s", err.Error())
+			}
+
+			if !matched {
+				t.Errorf("incorrect body, received: %s", response.Body)
+			}
+		})
+	}
+}
