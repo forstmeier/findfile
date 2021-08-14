94ff3e50af0a8008351d78c8670a12540124140c
forstmeier <john.forstmeier@gmail.com>
2021-08-14 13:52:42 -0400

    Adjust printing verb

--- util/util.go
+++ util/util.go
@@ -4,5 +4,5 @@ import "log"
 
 // Log provides a basic wrapper to format log output.
 func Log(key string, value interface{}) {
-	log.Printf(`{"%s": "%v"}`, key, value)
+	log.Printf(`{"%s": "%+v"}`, key, value)
 }
