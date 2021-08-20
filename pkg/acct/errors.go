d59595864085f5a3e14d81887d0a87404f12a5aa
forstmeier <john.forstmeier@gmail.com>
2021-08-20 13:29:18 -0400

    Update error messages in acct package

--- pkg/acct/errors.go
+++ pkg/acct/errors.go
@@ -11,7 +11,7 @@ type ErrorPutItem struct {
 }
 
 func (e *ErrorPutItem) Error() string {
-	return fmt.Sprintf("%s: create account: %s", packageName, e.err.Error())
+	return fmt.Sprintf("[%s] [create account]: %s", packageName, e.err.Error())
 }
 
 // ErrorGetItem wraps errors returned by dynamodb.DynamoDB.GetItem
@@ -21,7 +21,7 @@ type ErrorGetItem struct {
 }
 
 func (e *ErrorGetItem) Error() string {
-	return fmt.Sprintf("%s: read account: %s", packageName, e.err.Error())
+	return fmt.Sprintf("[%s] [read account]: %s", packageName, e.err.Error())
 }
 
 // ErrorIncorrectValue wraps errors returned by checkValues in the
@@ -31,7 +31,7 @@ type ErrorIncorrectValue struct {
 }
 
 func (e *ErrorIncorrectValue) Error() string {
-	return fmt.Sprintf("%s: update account: incorrect key [%s] in values", packageName, e.key)
+	return fmt.Sprintf("[%s] [update account]: incorrect key [%s] in values", packageName, e.key)
 }
 
 // ErrorUpdateItem wraps errors returned by dynamodb.DynamoDB.UpdateItem
@@ -41,7 +41,7 @@ type ErrorUpdateItem struct {
 }
 
 func (e *ErrorUpdateItem) Error() string {
-	return fmt.Sprintf("%s: update account: %s", packageName, e.err.Error())
+	return fmt.Sprintf("[%s] [update account]: %s", packageName, e.err.Error())
 }
 
 // ErrorDeleteItem wraps errors returned by dynamodb.DynamoDB.DeleteItem
@@ -51,5 +51,5 @@ type ErrorDeleteItem struct {
 }
 
 func (e *ErrorDeleteItem) Error() string {
-	return fmt.Sprintf("%s: delete account: %s", packageName, e.err.Error())
+	return fmt.Sprintf("[%s] [delete account]: %s", packageName, e.err.Error())
 }
--- pkg/acct/errors_test.go
+++ pkg/acct/errors_test.go
@@ -9,7 +9,7 @@ func TestErrorPutItem(t *testing.T) {
 	err := &ErrorPutItem{err: errors.New("mock put item error")}
 
 	recieved := err.Error()
-	expected := "acct: create account: mock put item error"
+	expected := "[acct] [create account]: mock put item error"
 
 	if recieved != expected {
 		t.Errorf("incorrect error message, received: %s, expected: %s", recieved, expected)
@@ -20,7 +20,7 @@ func TestErrorGetItem(t *testing.T) {
 	err := &ErrorGetItem{err: errors.New("mock get item error")}
 
 	recieved := err.Error()
-	expected := "acct: read account: mock get item error"
+	expected := "[acct] [read account]: mock get item error"
 
 	if recieved != expected {
 		t.Errorf("incorrect error message, received: %s, expected: %s", recieved, expected)
@@ -31,7 +31,7 @@ func TestErrorIncorrectValue(t *testing.T) {
 	err := &ErrorIncorrectValue{key: "test_key"}
 
 	recieved := err.Error()
-	expected := "acct: update account: incorrect key [test_key] in values"
+	expected := "[acct] [update account]: incorrect key [test_key] in values"
 
 	if recieved != expected {
 		t.Errorf("incorrect error message, received: %s, expected: %s", recieved, expected)
@@ -42,7 +42,7 @@ func TestErrorUpdateItem(t *testing.T) {
 	err := &ErrorUpdateItem{err: errors.New("mock update item error")}
 
 	recieved := err.Error()
-	expected := "acct: update account: mock update item error"
+	expected := "[acct] [update account]: mock update item error"
 
 	if recieved != expected {
 		t.Errorf("incorrect error message, received: %s, expected: %s", recieved, expected)
@@ -53,7 +53,7 @@ func TestErrorDeleteItem(t *testing.T) {
 	err := &ErrorDeleteItem{err: errors.New("mock delete item error")}
 
 	recieved := err.Error()
-	expected := "acct: delete account: mock delete item error"
+	expected := "[acct] [delete account]: mock delete item error"
 
 	if recieved != expected {
 		t.Errorf("incorrect error message, received: %s, expected: %s", recieved, expected)
