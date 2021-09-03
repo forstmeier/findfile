63222614b701d1c1c9a3b6c107344dbf3c417877
forstmeier <john.forstmeier@gmail.com>
2021-09-03 12:32:36 -0400

    Adjust parser logic with temporary query

--- pkg/cql/parser.go
+++ pkg/cql/parser.go
@@ -1,16 +1,13 @@
 package cql
 
 import (
-	"encoding/json"
+	"context"
 	"errors"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
-	errorKeyNotSupported      = errors.New("search object key received not supported")
 	errorTooManyAttributes    = errors.New("search object contains too many attributes")
+	errorKeyNotSupported      = errors.New("search object must be under \"search\" key")
 	errorTypeIncorrect        = errors.New("search object must be search object type")
 	errorMissingText          = errors.New("search object must contain text")
 	errorPageNumberZero       = errors.New("search object page number must not be \"0\"")
@@ -24,108 +21,28 @@ type search struct {
 	Coordinates [2][2]float64 `json:"coordinates"`
 }
 
-func parseCQL(accountID string, cqlQuery map[string]interface{}) ([]byte, error) {
-	var output []byte
-
+func parseCQL(ctx context.Context, accountID string, cqlQuery map[string]interface{}) ([]byte, error) {
 	if len(cqlQuery) > 1 {
 		return nil, errorTooManyAttributes
 	}
-	if searchValue, searchOK := cqlQuery["search"]; searchOK {
-		searchJSON, typeOK := searchValue.(search)
-		if !typeOK {
-			return nil, errorTypeIncorrect
-		}
 
-		if err := validateSearchJSON(searchJSON); err != nil {
-			return nil, err
-		}
-
-		bsonQuery := newBSONQuery(
-			accountID,
-			searchJSON.PageNumber,
-			searchJSON.Text,
-			searchJSON.Coordinates,
-		)
-
-		var err error
-		output, err = json.Marshal(bsonQuery)
-		if err != nil {
-			return nil, err
-		}
-
-	} else {
+	searchValue, searchOK := cqlQuery["search"]
+	if !searchOK {
 		return nil, errorKeyNotSupported
 	}
 
-	return output, nil
-}
-
-func newBSONQuery(accountID string, pageNumber int64, text string, coordinates [2][2]float64) bson.D {
-	return bson.D{
-		primitive.E{
-			Key:   "account_id",
-			Value: accountID,
-		},
-		primitive.E{
-			Key: "pages",
-			Value: bson.D{
-				primitive.E{
-					Key:   "page_number",
-					Value: pageNumber,
-				},
-				primitive.E{
-					Key: "lines",
-					Value: bson.D{
-						primitive.E{
-							Key: "$text",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$search",
-									Value: text,
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.top_left.x",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$lte",
-									Value: coordinates[1][0],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.bottom_right.x",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$gte",
-									Value: coordinates[0][0],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.top_right.y",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$gte",
-									Value: coordinates[1][1],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.bottom_left.y",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$lte",
-									Value: coordinates[0][1],
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+	searchJSON, typeOK := searchValue.(search)
+	if !typeOK {
+		return nil, errorTypeIncorrect
 	}
+
+	if err := validateSearchJSON(searchJSON); err != nil {
+		return nil, err
+	}
+
+	query := []byte("TEMP")
+
+	return query, nil
 }
 
 func validateSearchJSON(input search) error {
--- pkg/cql/parser_test.go
+++ pkg/cql/parser_test.go
@@ -2,12 +2,8 @@ package cql
 
 import (
 	"bytes"
-	"encoding/json"
-	"reflect"
+	"context"
 	"testing"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Test_parseCQL(t *testing.T) {
@@ -63,110 +59,19 @@ func Test_parseCQL(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			received, err := parseCQL("account_id", test.input)
+			received, err := parseCQL(context.Background(), "account_id", test.input)
 
 			if err != nil {
 				if err != test.error {
 					t.Errorf("incorrect error, received: %s, expected: %s", err.Error(), test.error.Error())
 				}
 			} else {
-				coordinates := [2][2]float64{
-					{0.1, 0.1},
-					{0.5, 0.5},
-				}
-
-				bsonQuery := newBSONQuery("account_id", 1, "lookup text", coordinates)
-				expected, err := json.Marshal(bsonQuery)
-				if err != nil {
-					t.Fatalf("error creating expected bson query: %s", err.Error())
-				}
-
+				expected := []byte("TEMP")
 				if bytes.Compare(received, expected) != 0 {
 					t.Errorf("incorrect byptes, received: %v, expected: %v", received, expected)
 				}
 			}
 		})
-	}
-}
-
-func Test_newBSONQuery(t *testing.T) {
-	pageNumber := int64(1)
-	text := "lookup text"
-	coordinates := [2][2]float64{
-		{0.1, 0.1},
-		{0.5, 0.5},
-	}
-
-	received := newBSONQuery("account_id", pageNumber, text, coordinates)
-
-	expected := bson.D{
-		primitive.E{
-			Key:   "account_id",
-			Value: "account_id",
-		},
-		primitive.E{
-			Key: "pages",
-			Value: bson.D{
-				primitive.E{
-					Key:   "page_number",
-					Value: pageNumber,
-				},
-				primitive.E{
-					Key: "lines",
-					Value: bson.D{
-						primitive.E{
-							Key: "$text",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$search",
-									Value: text,
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.top_left.x",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$lte",
-									Value: coordinates[1][0],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.bottom_right.x",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$gte",
-									Value: coordinates[0][0],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.top_right.y",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$gte",
-									Value: coordinates[1][1],
-								},
-							},
-						},
-						primitive.E{
-							Key: "coordinates.bottom_left.y",
-							Value: bson.D{
-								primitive.E{
-									Key:   "$lte",
-									Value: coordinates[0][1],
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	if !reflect.DeepEqual(received, expected) {
-		t.Errorf("incorrect query, received: %+v, expected: %+v", received, expected)
 	}
 }
 
