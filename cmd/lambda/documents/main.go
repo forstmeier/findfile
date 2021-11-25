d89b4681feb853a7b1a4e9855a769f0adfbf07d9
forstmeier <john.forstmeier@gmail.com>
2021-11-25 12:41:20 -0500

    Add main function for documents Lambda

--- cmd/lambda/documents/main.go
+++ cmd/lambda/documents/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/forstmeier/findfile/pkg/db"
+)
+
+func main() {
+	newSession := session.New()
+
+	evtClient, err := db.New(
+		newSession,
+	)
+	if err != nil {
+		panic(fmt.Sprintf("error creating db client: %v", err))
+	}
+
+	httpSecurityHeader := os.Getenv("HTTP_SECURITY_HEADER")
+	httpSecurityKey := os.Getenv("HTTP_SECURITY_KEY")
+
+	lambda.Start(handler(evtClient, httpSecurityHeader, httpSecurityKey))
+}
