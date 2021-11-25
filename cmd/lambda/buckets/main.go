3f46ba3ab7d888ecb342441ffe03856ca4f591bb
forstmeier <john.forstmeier@gmail.com>
2021-11-25 11:43:40 -0500

    Add main function for buckets Lambda

--- cmd/lambda/buckets/main.go
+++ cmd/lambda/buckets/main.go
@@ -1,3 +1,26 @@
 //+build !test
 
 package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/forstmeier/findfile/pkg/evt"
+)
+
+func main() {
+	newSession := session.New()
+
+	evtClient := evt.New(
+		newSession,
+		os.Getenv("TRAIL_NAME"),
+	)
+
+	httpSecurityHeader := os.Getenv("HTTP_SECURITY_HEADER")
+	httpSecurityKey := os.Getenv("HTTP_SECURITY_KEY")
+
+	lambda.Start(handler(evtClient, httpSecurityHeader, httpSecurityKey))
+}
